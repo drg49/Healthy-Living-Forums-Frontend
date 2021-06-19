51a1f31864814ffbf74edddb818f550e6f75eec0
Danny Gavin <homevpn14@gmail.com>
2021-06-19 09:12:19 -0700

    Added the delete and edit icon for each comment. Edit icon has no functionality yet.

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,6 +1,32 @@
+import { useContext } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {GlobalCtx} from "../App"
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons'
 const moment = require('moment')
 
+const trash = <FontAwesomeIcon icon={faTrashAlt} />
+const edit = <FontAwesomeIcon icon={faEdit} />
+
 const Comments = ({postsToShow}) => {
+
+    const currentUser = localStorage.getItem("user")    
+
+    const {gState} = useContext(GlobalCtx)
+    const {url, token} = gState
+
+    const handleDelete = (id) => {
+        fetch(`${url}/comments/${id}`, {
+            method: "delete",
+            headers: {
+                "Authorization": "bearer " + token
+            }
+        }).then(() => window.location.reload())
+    }
+
+    const beginUpdate = () => {
+        
+    }
+
     return (
         <section>
             {postsToShow.map((item) => {
@@ -11,6 +37,10 @@ const Comments = ({postsToShow}) => {
                             <p>{moment(item.created_at).format('l')}</p>
                         </div>
                         <p>{item.body}</p>
+                        {currentUser === item.author ? <div id="delete-div">
+                        <button onClick={()=> beginUpdate(item.id, item.body)}>{edit}</button>
+                        <button onClick={()=> handleDelete(item.id)}>{trash}</button>
+                        </div> : null}
                     </div>
                 )
             })}
