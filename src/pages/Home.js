62d832a215fea51ff124ee4afbbde57a348858e4
Danny Gavin <homevpn14@gmail.com>
2021-06-20 17:35:25 -0700

    fixed typos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
             </section>
             <article>
                 <h2 id="home-h2">Making the World a Healthier Place</h2>
-                <p id="home-p">While browsing our forums, you will realize that every user's goal is to become healthier. The Healthy Living Forum is a close community where people can talk about their health with other individuals. There are plenty of discussion topics to choose from where you can start your own discussion, or simply join in on one. The Healthy Living Forums should not be a place to seek medical advice but instead acts as a place where individuals can support each other through their time of need.</p>
+                <p id="home-p">While browsing our forums, you will realize that every user's goal is to become healthier. The Healthy Living Forums is a close community where people can talk about their health with other individuals. There are plenty of discussion topics to choose from where you can start your own discussion, or simply join in on one. The Healthy Living Forums should not be a place to seek medical advice but instead acts as a place where individuals can support each other through their time of need.</p>
             </article>
         </div>
     )
